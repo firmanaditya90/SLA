e1941cfda3ac55c423ee2001c63f500e2de27e46
firmanaditya90 <firman.aditya.baskoro@gmail.com>
2025-08-14 14:38:18 +0700

    Update sla_app.py

--- sla_app.py
+++ sla_app.py
@@ -25,6 +25,16 @@ st.set_page_config(page_title="SLA Payment Analyzer", layout="wide", page_icon="
 # ------------------------------
 
 # ==============================
+# Logo di Sidebar
+# ==============================
+with st.sidebar:
+    st.image(
+        asdp_logo.png",
+        width=200
+    )
+    st.markdown("<h3 style='text-align: center;'>🔒 Admin Panel</h3>", unsafe_allow_html=True)
+
+# ==============================
 # Styling: CSS untuk look modern
 # ==============================
 st.markdown("""
@@ -250,6 +260,9 @@ st.markdown(f'<div class="small">Menampilkan data periode dari <b>{start_periode
 
 available_sla_cols = [col for col in sla_cols if col in df_filtered.columns]
 proses_grafik_cols = [c for c in ["FUNGSIONAL", "VENDOR", "KEUANGAN", "PERBENDAHARAAN"] if c in available_sla_cols]
+
+# Created by
+st.sidebar.markdown("<p style='text-align:center; font-size:12px; color:gray;'>Created by. Firman Aditya</p>", unsafe_allow_html=True)
 
 # ==============================
 # Parsing SLA setelah filter (dengan status)
