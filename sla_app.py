c789f676ac9284510ba8105154c0b3f50b715b42
firmanaditya90 <firman.aditya.baskoro@gmail.com>
2025-08-16 17:03:13 +0700

    Update sla_app.py

--- sla_app.py
+++ sla_app.py
@@ -632,7 +632,7 @@ image_url = "https://github.com/firmanaditya90/SLA/blob/main/Captain%20Ferizy.pn
 # ==========================================================
 #                       Streamlit Tabs
 # ==========================================================
-tab_poster, tab_pdf = st.tabs(["📥 Download Poster", "📥 Download PDF"])
+tab_report = st.tabs(["📥 Download Poster", "📥 Download PDF"])
 
 # ---------------- Tab Poster ----------------
 with tab_poster:
