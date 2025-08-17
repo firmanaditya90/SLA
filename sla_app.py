e41ba0a95a74599c7ac872c8871a001074738bfc
firmanaditya90 <firman.aditya.baskoro@gmail.com>
2025-08-17 21:02:05 +0700

    Update sla_app.py

--- sla_app.py
+++ sla_app.py
@@ -673,14 +673,22 @@ def generate_poster_A4(sla_text_dict, rata_proses_seconds, df_proses, image_url,
         pos_y = card_top + table_img.height + 60
         bg.paste(kemudi_img, (pos_x, pos_y), kemudi_img)
 
-        font_target = ImageFont.truetype(os.path.join(os.path.dirname(__file__), "Anton-Regular.ttf"), 100)
+        # Pakai font judul, ukuran besar
+        font_target = ImageFont.truetype(
+            os.path.join(os.path.dirname(__file__), "Anton-Regular.ttf"), 150
+        )
         text = "ON TARGET"
         tw, th = draw.textsize(text, font=font_target)
+
+        # Center terhadap kemudi, sedikit lebih tinggi biar tidak jatuh ke bawah
         text_x = pos_x + (kemudi_img.width - tw) // 2
-        text_y = pos_y + kemudi_img.height + 20
+        text_y = pos_y + kemudi_img.height - th - 10   # naikkan posisi
+
+        # Warna hijau tebal
         draw.text((text_x, text_y), text, fill=(0, 150, 0), font=font_target)
     except Exception as e:
         print("Gagal render Kemudi/On Target:", e)
+        
     
     # ---------- Captain Ferizy ----------
     try:
